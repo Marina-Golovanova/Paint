82331050eb3adad14dfa4283830d06a425411831
Marina <gma18@list.ru>
2021-08-04 01:24:15 +0300

    feat: add cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,74 +1,37 @@
 import React from "react";
-import { DrawContainer } from "./DrawContainer";
+import { DrawContainer } from "./editor/DrawContainer";
+import { Tools } from "./editor/Tools";
+import { ControlPanel } from "./editor/ControlPanel";
 
 import "./style.scss";
 
 export function App(): JSX.Element {
-  const colors = ["#f00", "#ff0", "f0f", "#0ff", "#0f0", "#00f"];
+  const colors = ["#f00", "#ff0", "#f0f", "#0ff", "#0f0", "#00f"];
   const [settedColorIndex, setSettedColorIndex] = React.useState(0);
   const [selectedBrushWidth, setSelectedBrushWidth] = React.useState(5);
+
+  const onSettedColorIndex = (index: number) => {
+    setSettedColorIndex(index);
+  };
+
+  const onSelectedBrushWidth = (width: number) => {
+    setSelectedBrushWidth(width);
+  };
+
   return (
     <div>
       <ControlPanel />
       <Tools
         colors={colors}
-        onColorSelect={(index) => setSettedColorIndex(index)}
+        onColorSelect={onSettedColorIndex}
         initialSelectedIndex={settedColorIndex}
         initialBrushWidth={selectedBrushWidth}
-        onBrushWidthSelect={(width) => setSelectedBrushWidth(width)}
+        onBrushWidthSelect={onSelectedBrushWidth}
       />
       <DrawContainer
         selectedColor={colors[settedColorIndex]}
         brushWidth={selectedBrushWidth}
       />
-    </div>
-  );
-}
-
-interface IToolsProps {
-  colors: string[];
-  onColorSelect: (index: number) => void;
-  initialSelectedIndex: number;
-  onBrushWidthSelect: (width: number) => void;
-  initialBrushWidth: number;
-}
-
-export function Tools(props: IToolsProps): JSX.Element {
-  const colorButtons = props.colors.map((color, index) => (
-    <button
-      className={`colorButton ${
-        index === props.initialSelectedIndex ? "colorButton--selected" : ""
-      }`}
-      style={{ backgroundColor: color }}
-      onClick={() => {
-        props.onColorSelect(index);
-      }}
-      key={color}
-    ></button>
-  ));
-
-  return (
-    <div className="tools-wrapper">
-      <div>
-        Цвет:
-        {colorButtons}
-      </div>
-      <div>
-        Кисть:
-        <input
-          value={props.initialBrushWidth}
-          type="range"
-          onChange={(ev) => props.onBrushWidthSelect(ev.target.valueAsNumber)}
-        />
-      </div>
-    </div>
-  );
-}
-
-export function ControlPanel(): JSX.Element {
-  return (
-    <div className="controlPanel-wrapper">
-      <input type="file" />
     </div>
   );
 }
---
+++ src/editor/ControlPanel.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function ControlPanel(): JSX.Element {
+  return (
+    <div className="controlPanel-wrapper">
+      <input type="file" />
+    </div>
+  );
+}
